b341a6f9c2fc843df6395e427add72200ba75c74
Grady Robbins <gradyrobbins13@gmail.com>
2019-03-08 12:02:58 -0600

    template files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ch8_studentExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
